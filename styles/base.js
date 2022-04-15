fa8284a896890b3083ebedecc9416a1b608bdde7
Pablo Silva <pablos.epost@gmail.com>
2022-04-15 19:43:39 +0100

    added style file

---
+++ styles/base.js
@@ -0,0 +1,48 @@
+export const container = {
+    flex: 1,
+    backgroundColor: '#4b7d8c',
+};
+
+
+export const base = {
+    flex: 1,
+    // alignItems: 'center',
+    paddingLeft: 12,
+    paddingRight: 12,
+    // bottom: 20,
+};
+
+
+export const image = {
+    width: 400,
+    height: 240,
+    marginBottom: 25,
+};
+
+
+export const buttonContainer = {
+    elevation: 8,
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    marginBottom: 24,
+    marginRight: 35,
+    marginLeft: 35,
+};
+
+export const TabBar = {
+    backgroundColor: '#345762',
+    // backgroundColor: 'blue',
+    position: 'absolute',
+    // bottom: 10,
+    left: 20,
+    right: 20,
+    elevation: 0,
+    borderRadius: 15,
+    height: 50,
+    // padding: 10,
+    
+}
+
+
---
+++ styles/typography.js
@@ -0,0 +1,48 @@
+export const header1 = {
+    fontSize: 42,
+    marginBottom: 28,
+};
+
+export const header2 = {
+    fontSize: 34,
+    marginBottom: 28,
+    textAlign: 'center',
+};
+
+export const header3 = {
+    fontSize: 28,
+    marginBottom: 28,
+    marginTop: 14,
+};
+
+
+export const data = {
+    color: '#333',
+    fontSize: 16,
+    lineHeight: 24,
+    marginBottom: 24,
+}
+
+export const normal = {
+    fontSize: 20,
+    marginBottom: 28,
+};
+
+
+export const buttonText = {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+};
+
+
+export const listInfo = {
+    fontSize: 16,
+    marginBottom: 5, 
+};
+
+export const Topspace = {
+    marginTop: 14,
+};
